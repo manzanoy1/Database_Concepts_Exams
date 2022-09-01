73f469d8b63acbe3e1d228512e3eff42e24a2b41
Yanira Manzano <56289361+manzanoy1@users.noreply.github.com>
2022-08-31 20:18:19 -0400

    Add files via upload

---
+++ HawkSoftware.sql
@@ -0,0 +1,41 @@
+-- Yanira Manzano
+-- Midterm Part 1-B
+
+CREATE DATABASE HawkSoftware;
+use HawkSoftware;
+
+CREATE TABLE LAPTOP(
+	Laptop_ID		Int				NOT NULL auto_increment,
+    Laptop_Make		Date			NOT NULL,
+    Laptop_Model	VarChar(25)		NOT NULL,
+	CONSTRAINT		LAPTOP_PK	PRIMARY KEY(Laptop_ID)
+	);
+    
+CREATE TABLE SOFTWARE(
+	Software_ID		Int				NOT NULL auto_increment,
+    Package_Type	VarChar(30)		NOT NULL,
+    Package_Ver 	Char(30)		NOT NULL,
+    CONSTRAINT		SOFTWARE_PK		PRIMARY KEY(Software_ID),
+    CONSTRAINT	SOFTWARE_Package_Type
+					CHECK (Package_Type IN ('Windows',
+							'Mac', 'Linux')),
+	CONSTRAINT 			SOFTWARE_LAPTOP_FK FOREIGN KEY(Laptop_ID)
+							REFERENCES LAPTOP(Laptop_ID)
+								ON UPDATE NO ACTION
+								ON DELETE NO ACTION
+	);
+    
+CREATE TABLE FACULTY(
+	Hawk_ID			Int			   NOT NULL auto_increment,
+    First_Name		Char(25)	   NOT NULL,
+    Last_Name		Char(25)	   NOT NULL,
+    CONSTRAINT FACULTY_PK	PRIMARY KEY(Hawk_ID),
+    CONSTRAINT 			FACULTY_LAPTOP_FK FOREIGN KEY(Laptop_ID)
+							REFERENCES LAPTOP(Laptop_ID)
+								ON UPDATE NO ACTION
+								ON DELETE NO ACTION,
+	CONSTRAINT 			FACULTY_SOFTWARE_FK FOREIGN KEY(Software_ID)
+							REFERENCES SOFTWARE(Software_ID)
+								ON UPDATE NO ACTION
+								ON DELETE NO ACTION
+    );
---
+++ Midterm_Part2_YM.sql
@@ -0,0 +1,88 @@
+-- Yanira Manzano
+-- 03/25/2022
+-- Midterm Part 2
+
+use ap;
+
+-- Q1
+SELECT
+	*
+FROM
+	heather_design.columns;
+
+-- Q2
+SELECT 
+	* 
+FROM 
+	invoices;
+    
+-- Q3
+SELECT 
+	invoice_date AS 'mm-dd-yyyy'
+FROM 
+	invoices;
+    
+-- Q4
+SELECT
+	CONCAT(FirstName, ' ', LastName) AS FullName,
+    CONCAT(LEFT(FirstName, 1),('.'),
+    LEFT(LastName, 1),('.')) AS Initials,
+    StreetAddress as Address
+FROM 
+	heather_design.customer;
+    
+-- Q5
+SELECT
+	FirstName,
+    LastName
+FROM
+	heather_design.customer
+WHERE
+	LastName LIKE 'E_n%';
+    
+-- Q6
+SELECT
+	FirstName,
+    LastName,
+    EmailAddress,
+    City
+FROM
+    heather_design.customer
+WHERE
+	City LIKE 'San%' and
+    FirstName like 'S%';
+    
+-- Q7
+SELECT
+	InvoiceNumber
+FROM
+	heather_design.line_item;
+    
+-- Q8
+SELECT
+	SUM(Total) AS InvoiceTotal,
+    AVG(Total) AS InvoiceAvgTotal
+FROM
+	heather_design.invoice;
+    
+-- Q9
+SELECT
+	InvoiceNumber,
+    PaymentType,
+    SUM(SubTotal - Shipping) AS 'PriceBeforeTax'
+FROM 
+	heather_design.invoice;
+    
+-- Q10
+SELECT
+	InvoiceDate,
+    SUM(Total) AS 'Sales'
+FROM
+	heather_design.invoice
+WHERE
+	InvoiceDate BETWEEN '01/01/2019' AND '12/31/2019'
+GROUP BY
+	InvoiceDate, Total WITH ROLLUP
+    
+	
+	
